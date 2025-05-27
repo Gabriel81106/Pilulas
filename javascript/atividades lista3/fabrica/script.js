9690c1b0aaffaacdfffe1e34fc570e49f8584bc1
Gabriel81106 <gabriel81106@edu.unipar.br>
2025-05-27 20:37:43 -0300

    Add files via upload

--- javascript/atividades lista3/fabrica/script.js
+++ javascript/atividades lista3/fabrica/script.js
@@ -1,17 +1,14 @@
-let input = document.querySelector("#tempo");
+let dias = document.querySelector("#tempo");
 let botao = document.querySelector("#botao");
 let resultado = document.querySelector("#resultado");
 
-function calcular() {
-    let dias = Number(input.value);
-
-    if(dias > 30){
-
-
-    }
-
+function calcular(){
+    let anos = Number(dias.value) / 360;
+    let resto = Number(dias.value) % 360;
+    let meses = resto / 30;
+    let diasRestantes = resto % 30;
     
-
+    resultado.textContent = diasRestantes + " dias, " + meses.toFixed(1) + " meses e " + anos.toFixed(2) + " anos.";
 }
 
 botao.onclick = function(){
